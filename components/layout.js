7b946406117628c1f241f6742a8648fa14eac00b
lxzxl <lxz318@aliyun.com>
2018-02-20 14:33:33 +0800

    remove nav.

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,10 @@
 import '../styles.scss';
 import Head from './head';
-import Nav from './nav';
 const Fragment = React.Fragment;
 
 const Layout = props => (
   <Fragment>
     <Head {...props} />
-    <Nav />
     <section className="section">
       <div className="container">{props.children}</div>
     </section>
